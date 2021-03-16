61c508ecc2b092e7900193a23da081790107b965
koffiekasper <vdpolkasper@gmail.com>
2021-03-16 14:36:21 +0000

    fixed git push

--- pir-sensor-time.sh
+++ pir-sensor-time.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+record_photos () {
+	for i in {1..$1};
+	do
+		filename="./images/$2.jpg" 
+		fswebcam $filename
+	done
+}
+
+
 
 if [ ! -d /sys/class/gpio/gpio23/ ]
 then
@@ -12,11 +21,9 @@ do
 	movement=`cat /sys/class/gpio/gpio23/value`
 	if [ $movement == '1' ]
 	then
-		moment=`date +"%D %T"`
+		moment=`date +"%F-%T"`
+		record_photos 10 $moment
 		echo "Movement detected on "$moment
-		echo $moment >> times.txt
-
-		sleep 2
 	fi
-	sleep 1
+	sleep 0.1
 done
